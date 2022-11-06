fd940e762c64b44408d632314adcc1151db1d7e9
Olga Zarosylova <zoa1906@gmail.com>
2022-11-06 20:26:46 +0200

    //lf

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        toBeDivided= toBeDivided/5;
+        System.out.println(toBeDivided);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider numberDivider = new NumberDivider();
+        numberDivider.divide(10);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    float minutes;
+    public static void main(String[] args) {
+        TimeConvertor timeConvertor = new TimeConvertor();
+        for(int i = 1; i<60; i++) {
+
+            timeConvertor.convert(i);
+        }
+    }
     public void convert(float minutes) {
+        float sec = minutes*60;
+        System.out.println(sec);
     }
 }
